bd523edba94c2f930b747fcef6dd70e0601015ce
Matt Clark <clark.matthewr@gmail.com>
2020-11-04 17:29:46 -0600

    set element bindings

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
 let App = {
   user: null,
 
+  userFirstName: function() {
+    return this.user.displayName.split(' ')[0];
+  },
+
   signup: function(data) {
     firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
-      .then(function() {
-        redirect('profile/user-profile');
-      })
-      .catch(function(error) {});
+      .then(redirectToProfile)
+      .catch(logError);
   },
   signin: function(data) {
     firebase.auth().signInWithEmailAndPassword(data.email, data.password)
-      .then(function() {
-        redirect('profile/user-profile');
-      })
-      .catch(function(error) {});
+      .then(redirectToProfile)
+      .catch(logError);
   },
   signinGoogle: function() {},
   signout: function() {
     firebase.auth().signOut();
+    redirectToHome();
   },
 
-  putInStorage: function(uid, file) {},
-  getFromStorage: function(uid, list) {},
+  putFileInStorage: function(uid, file) {},
+  getFileFromStorage: function(uid, list) {},
 
-  putInDatabase: function(uid, data) {},
-  getFromDatabase: function(uid, list) {},
+  putDataInDatabase: function(uid, data) {},
+  getDataFromDatabase: function(uid, list) {},
 
   setAuthStateListener: function() {
     firebase.auth().onAuthStateChanged(function(user) {
@@ -55,8 +56,9 @@ let App = {
   },
   toggleNavUserLoggedInWithoutPhoto: function() {
     this.$loginButton.toggle(false);
-    // set name in profile name button
-    this.$profileAvatarButton.toggle(false);
+    // load a default placeholder photo as avatar
+    this.$profileAvatarButton.toggle(true);
+    this.$profileNameButton.text(this.userFirstName());
     this.$profileNameButton.toggle(true);
   },
   toggleNavUserLoggedOut: function() {
@@ -67,12 +69,60 @@ let App = {
   },
 
   bindElements: function() {
+    // nav
     this.$loginButton = $('#login-button');
     this.$profileAvatarButton = $('#profile-avatar-button');
-    this.$profileNameButton = $('#profile-name-button')
+    this.$profileNameButton = $('#profile-name-button');
+    this.$profileAvatarNameSection = $('#profile-avatar-and-name');
+
+    // sign up
+    this.$signupForm = $('#signupForm');
+    this.$signupEmail = $('#signupEmail');
+    this.$signupPassword = $('#signupPassword');
+    this.$signupAgreeToTermsCheckbox = $('#sigupCheckbox');
+
+    // sign in
+    this.$signinForm = $('#signinForm');
+    this.$signinGoogleButton = $('#signin-google-button');
+    this.$signinEmail = $('#signinEmail');
+    this.$signinPassword = $('#signinPassword');
+    this.$signinRememberMeCheckbox = $('#signinCheckbox');
+    this.$forgotPasswordLink = $('#forgotPasswordLink');
+
+    // profile general
+    this.$usernameWelcome = $('#username-welcome');
+    this.$backgroundHeaderImage = $('#background-header-image');
+    this.$userAvatar = $('#user-avatar');
+    this.$usernameHeader = $('#username-header');
+
+    // profile about
+    this.$aboutPhone = $('#about-phone');
+    this.$aboutBirthdate = $('#about-birthdate');
+    this.$aboutContactEmail = $('#about-contact-email');
+    this.$aboutLocation = $('#about-location');
+    this.$aboutPosition = $('#about-position');
+    this.$aboutBio = $('#about-bio');
+
+    // profile edit
+    this.$editProfileForm = $('wf-form-profile');
+    this.$editPhotoUpload = $('#edit-photo-upload');
+    this.$editFirstName = $('#edit-first-name');
+    this.$editLastName = $('#edit-last-name');
+    this.$editBirthdateDay = $('#edit-birthdate-day');
+    this.$editBirthdateMonth = $('#edit-birthdate-month');
+    this.$editBirthdateYear = $('#edit-birthdate-year');
+    this.$editLocation = $('#edit-location');
+    this.$editContactEmail = $('#edit-contact-email');
+    this.$editPhone = $('#edit-phone');
+    this.$editFacebookLink = $('#edit-facebook-link');
+    this.$editInstagramLink = $('#edit-instagram-link');
+    this.$editTwitterLink = $('#edit-twitter-link');
+    this.$editBio = $('#edit-bio');
   },
   bindEventListeners: function() {
-
+    // this.$signupForm.submit();
+    this.$signinForm.submit(handleSignin.bind(this));
+    this.$editProfileForm.submit(handleProfileEdit.bind(this));
   },
 
   handleSignup: function(event) {
@@ -85,6 +135,14 @@ let App = {
 
     this.signup(data);
   },
+  handleSignin: function(event) {
+    event.preventDefault();
+  },
+  handleProfileEdit: function(event) {
+    event.preventDefault();
+    let data = getEditProfileData();
+    
+  },
 
   init: function() {
     this.bindElements();
@@ -95,7 +153,8 @@ let App = {
 };
 
 $(function() {
-  window.app = App.init();
+  window.app = App.init(); // just for development
+  // App.init();  // production
 });
 
 function redirect(path) {
@@ -103,4 +162,17 @@ function redirect(path) {
     console.log('redirecting from ' + location.pathname + ' to ' + path);
     location.pathname = path;
   }
+}
+
+function redirectToHome() {
+  redirect('/');
+}
+
+function redirectToProfile() {
+  redirect('profile/user-profile');
+}
+
+function logError(error) {
+  console.error('Error code: ' + error.code);
+  console.error('Error message: ' + error.message);
 }
